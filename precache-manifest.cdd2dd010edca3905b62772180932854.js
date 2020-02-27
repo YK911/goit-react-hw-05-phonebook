19fd5a4fc430319c49ebc878b9c0cf4aabf25b2b
YK911 <jaroslavkositsa@gmail.com>
2020-02-27 15:49:48 +0200

    Updates

---
+++ precache-manifest.cdd2dd010edca3905b62772180932854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7f89e3b2d274710a5f5993b80ff8a7",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "e2ac4e0c3992bb3acf67",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.cc7b3ec6.chunk.css"
+  },
+  {
+    "revision": "61fa895f8dd20402abec",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.4713d391.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.4713d391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ac4e0c3992bb3acf67",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.c8d714ef.chunk.js"
+  },
+  {
+    "revision": "d5fa5ed8227b90d4a0a1",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.56026625.js"
+  }
+]);
